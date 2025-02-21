4cb08f27d67b1e2e219eb0b0dedd913b12fb5cad
anthonyouch <anthonyouch.programmer@gmail.com>
2025-02-22 08:09:11 +1100

    added framer motion scroll icon

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,6 +13,22 @@ const Hero = () => {
           <div id="text-behind-blur">ANTHONY<br /><span className="bottomText">OUCH</span></div>
           <div id="text-front">ANTHONY<br /><span className="bottomText">OUCH</span></div>
         </div>
+        
+        <div className="absolute bottom-1/8 w-full flex justify-center items-center">
+          <a href="">
+            <div className="w-[24px] h-[40px] rounded-3xl border-2 border-white opacity-70 flex justify-center">
+              <motion.div
+                animate={{ y: [0, 10, 0] }}
+                transition={{
+                  duration: 2,
+                  repeat: Infinity,
+                  repeatType: 'loop'
+                }}
+                className="w-1 h-2 rounded-full bg-white absolute top-[25%]"
+              />
+            </div>
+          </a>
+        </div>
     </section>
   )
 }
